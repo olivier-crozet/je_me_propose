e6857ca073775962952f4e6a41f963d11db81a01
leon-montagard <leon.mtg@aol.fr>
2020-04-22 23:28:31 +0200

    page connection

---
+++ connexion.php
@@ -0,0 +1,80 @@
+<header>
+
+<ul>
+				<li><a href="../index.php">Accueil</a></li>
+				<li><a href="inscription.php">Inscription</a></li>
+</ul>
+			
+</header>
+
+
+
+<?php	
+// --------------------------------------------DEBUT PHP--------------------------------------------
+session_start();
+
+if(!isset($_SESSION['id']))
+{
+if((isset($_POST['Valider']))&&(isset($_POST['champ1']))&&(isset($_POST['champ2'])))
+{
+
+	$connexion= mysqli_connect("localhost", "root", "", "je_me_propose"); 
+	$login=$_POST['champ1'];
+	$query="SELECT *from utilisateurs WHERE login='$login'";
+	$result= mysqli_query($connexion, $query);
+	$row = mysqli_fetch_array($result);
+	
+	if(password_verify($_POST['champ2'],$row['password'])) 
+	{
+	$_SESSION['login'] = $_POST['champ1'];
+	$_SESSION['password'] = $_POST['champ2'];
+	header ('location: index.php');
+	}
+	else
+	{	
+	?>
+	<div class="erreur">
+	<img src="../img/erreur.jpg" width="2%">
+	<div class="affichage">
+	<?php
+	echo "*Login ou mot de passe incorrect";	
+	?>
+	</div>
+	</div>
+	<?php
+	}
+}
+
+// --------------------------------------------FIN PHP--------------------------------------------
+?>
+	
+<section>
+	<div class="form-style-5">
+		<form method="post" action="connexion.php">
+		<fieldset>
+		<legend>Connexion</legend>	
+		<div class="input">
+		<input type="text" name="champ1" placeholder="Login *">
+		</div>
+		<div class="input">
+		<input type="password" name="champ2" placeholder="Password *">
+		</div>
+		<div class="input">
+		<input type="submit" name="Valider" value="SE CONNECTER"/>
+		</div>
+		</fieldset>
+		</form>
+	</div>
+</section>				
+
+<?php
+}
+else
+{
+	header('location: index.php');
+}
+?>	
+</body>
+
+
+</html>
